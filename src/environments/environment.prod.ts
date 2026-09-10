89d258d0744882d492f43e7e1400e3aa92065cc0
gutavovico <gustavosanzx11@gmail.com>
2026-09-10 06:25:05 -0400

    feat: ahora si deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+
+//export const environment = {
+//  production: true,
+//  apiUrl: 'https://tu-backend-en-produccion.com',
+//  inactivityTimeoutMinutes: 15
+//};
 export const environment = {
   production: true,
   apiUrl: 'https://telemedicina-backend-ezx9.onrender.com',
