0c0f7ff8f3ade861fda13d7665b3a35f52f5a141
gutavovico <gustavosanzx11@gmail.com>
2026-09-10 06:44:04 -0400

    deploy 2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
+const isBrowser = typeof window !== 'undefined';
+const isLocalhost = isBrowser && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1');
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000',
+  production: !isLocalhost,
+  apiUrl: isLocalhost ? 'http://localhost:8000' : 'https://telemedicina-backend-ezx9.onrender.com',
   inactivityTimeoutMinutes: 15
 };
+
