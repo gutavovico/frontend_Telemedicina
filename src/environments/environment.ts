d27b9a3f6fdd08443ad44827ea4d3aee32a1445a
jleonelf <rojasjorge2015@gmail.com>
2026-09-24 19:42:15 -0400

    enviroments render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://telemedicina-backend-ezx9.onrender.com',
   inactivityTimeoutMinutes: 15
 };
 
